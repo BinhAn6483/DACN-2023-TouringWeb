e3d49a2cb6cb1badc2d42519b84ccedd67d59ff1
Phan Văn Hùng <91454052+hungphan0501@users.noreply.github.com>
2023-04-04 15:22:44 +0700

    upload template home page and maven project

---
+++ TouringWeb/src/test/java/com/nhom10/touringweb/TouringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom10.touringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
