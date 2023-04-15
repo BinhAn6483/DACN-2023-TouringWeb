a839009712ef43ee9c0de9cf99061d9c4910ca50
Phan Văn Hùng <91454052+hungphan0501@users.noreply.github.com>
2023-04-15 12:46:03 +0700

    upload template and logic tour search

--- TouringWeb/src/main/java/com/nhom10/touringweb/controller/user/TourController.java
+++ TouringWeb/src/main/java/com/nhom10/touringweb/controller/user/TourController.java
@@ -4,16 +4,13 @@ import com.nhom10.touringweb.model.user.Tour;
 import com.nhom10.touringweb.service.LinkImgService;
 import com.nhom10.touringweb.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.Date;
 
 import java.util.HashMap;
 import java.util.List;
@@ -32,7 +29,7 @@ public class TourController {
 
     @GetMapping
     public ModelAndView getAll() {
-        List<Tour> list= tourService.getAll();
+        List<Tour> list = tourService.getAll();
         List<Tour> featuredTours = (List<Tour>) getListTourFeatured();
         List<Tour> listTourNew = (List<Tour>) getListTourNew();
         List<Tour> listTourDiscount = (List<Tour>) getListTourDiscount();
@@ -45,9 +42,40 @@ public class TourController {
         mav.addAllObjects(model);
         return mav;
     }
+
+    @GetMapping("/tours-search")
+    public ModelAndView getTourSearch(@RequestParam("location_name") String locationName, @RequestParam("start") String start, @RequestParam("end") String end) {
+        ModelAndView mav = new ModelAndView("tour_search_sidebar");
+        Map<String, Object> model = new HashMap<>();
+
+        Date dateStart = convertDate(start);
+        Date dateEnd = convertDate(end);
+        List<Tour> list = tourService.getToursBySearch(locationName, dateStart, dateEnd);
+        if (!list.isEmpty()) {
+            model.put("listTourSearch", list);
+            System.out.println(list.toString());
+        }
+        mav.addAllObjects(model);
+        return mav;
+    }
+
+    public Date convertDate(String start) {
+        Date date;
+        String[] s = start.split("/");
+        System.out.println(s[0] + "\t" + s[1] + "\t" + s[2]);
+        int day = Integer.parseInt(s[1]);
+        int month = Integer.parseInt(s[0])-1;
+        int year = Integer.parseInt(s[2])-1900;
+        date = new Date(year, month, day);
+        System.out.println(date);
+        return date;
+    }
+
+
+
     @GetMapping("/home")
     public ModelAndView home() {
-        List<Tour> list= tourService.getAll();
+        List<Tour> list = tourService.getAll();
         List<Tour> featuredTours = (List<Tour>) getListTourFeatured();
         List<Tour> listTourNew = (List<Tour>) getListTourNew();
         List<Tour> listTourDiscount = (List<Tour>) getListTourDiscount();
@@ -63,12 +91,12 @@ public class TourController {
 
 
     @GetMapping("/mainImgLink/{idTour}")
-    public String getMainImgLink(@PathVariable("idTour") Long idTour){
+    public String getMainImgLink(@PathVariable("idTour") Long idTour) {
         return linkImgService.getNameImgByIdTour(idTour);
     }
 
     @GetMapping("/{idTour}")
-    public ResponseEntity<Tour> getTourById(@PathVariable("idTour") Long idTour){
+    public ResponseEntity<Tour> getTourById(@PathVariable("idTour") Long idTour) {
         Tour tour = tourService.getTourById(idTour);
 
         return new ResponseEntity<Tour>(tour, HttpStatus.OK);
@@ -79,11 +107,11 @@ public class TourController {
     public ResponseEntity<List<Tour>> getListTourByStartingPoint(@PathVariable("startingPoint") String startingPoint) {
         try {
             List list = tourService.getAllTourByStartingPoint(startingPoint);
-            if(list.isEmpty()) {
+            if (list.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            return new ResponseEntity<List<Tour>>(list ,HttpStatus.OK);
-        }catch (Exception e) {
+            return new ResponseEntity<List<Tour>>(list, HttpStatus.OK);
+        } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -93,11 +121,11 @@ public class TourController {
     public List<Tour> getListTourFeatured() {
         try {
             List<Tour> featuredTours = tourService.getListTourFeatured();
-            if(featuredTours.isEmpty()) {
+            if (featuredTours.isEmpty()) {
                 return null;
             }
             return featuredTours;
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -106,11 +134,11 @@ public class TourController {
     public List<Tour> getListTourNew() {
         try {
             List<Tour> featuredTours = tourService.getListTourNew();
-            if(featuredTours.isEmpty()) {
+            if (featuredTours.isEmpty()) {
                 return null;
             }
             return featuredTours;
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -119,11 +147,11 @@ public class TourController {
     public List<Tour> getListTourDiscount() {
         try {
             List<Tour> featuredTours = tourService.getListTourDiscount();
-            if(featuredTours.isEmpty()) {
+            if (featuredTours.isEmpty()) {
                 return null;
             }
             return featuredTours;
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
