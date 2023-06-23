6cec7e521a7aed71ae1a84f95c1c2e2c4394d516
VuTru <19130250@st.hcmuaf.edu.vn>
2023-06-23 11:16:12 +0700

    change template managerCustomerDetail

--- TouringWeb/src/main/java/com/nhom10/touringweb/repository/BookingRepository.java
+++ TouringWeb/src/main/java/com/nhom10/touringweb/repository/BookingRepository.java
@@ -3,6 +3,7 @@ package com.nhom10.touringweb.repository;
 import com.nhom10.touringweb.model.user.Booking;
 import com.nhom10.touringweb.model.user.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
