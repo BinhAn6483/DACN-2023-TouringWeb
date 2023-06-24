62adb3ca33f7d74129c2b22f47905bf73ce9da1b
Phan Văn Hùng <91454052+hungphan0501@users.noreply.github.com>
2023-06-24 13:52:20 +0700

    update tour manage

--- TouringWeb/src/main/java/com/nhom10/touringweb/controller/admin/ToursManeger.java
+++ TouringWeb/src/main/java/com/nhom10/touringweb/controller/admin/ToursManeger.java
@@ -1,6 +1,9 @@
 package com.nhom10.touringweb.controller.admin;
 
 import com.nhom10.touringweb.model.user.Tour;
+import com.nhom10.touringweb.model.user.User;
+import com.nhom10.touringweb.model.user.WishList;
+import com.nhom10.touringweb.repository.TourRepository;
 import com.nhom10.touringweb.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,12 +11,18 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 import java.util.HashMap;
+
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 import java.util.Map;
 
@@ -23,13 +32,15 @@ public class ToursManeger {
     @Autowired
     TourService tourService;
 
+    @Autowired
+    TourRepository tourRepository;
 
 
     @GetMapping("/admin/tours-manage")
-    public ModelAndView getListTourManage(HttpServletRequest request, @RequestParam(name = "page",defaultValue = "0") int page) {
+    public ModelAndView getListTourManage(HttpServletRequest request, @RequestParam(name = "page", defaultValue = "0") int page) {
         ModelAndView mav = new ModelAndView("admin/dashboard-listTour");
         Map<String, Object> model = new HashMap<>();
-        int pageSize = 15;
+        int pageSize = 10;
 
         try {
 
@@ -47,7 +58,7 @@ public class ToursManeger {
             Page<Tour> listTours = tourService.getAll(pageable);
             if (!listTours.isEmpty()) {
                 model.put("listTour", listTours.getContent());
-                model.put("totalPages",listTours.getTotalPages()); //Thêm thuộc tính totalPages vào model
+                model.put("totalPages", listTours.getTotalPages()); //Thêm thuộc tính totalPages vào model
                 model.put("currentPage", page); //Thêm thuộc tính currentPage vào model
                 model.put("url", url);
                 mav.addAllObjects(model);
@@ -57,4 +68,18 @@ public class ToursManeger {
             e.printStackTrace();
         }
         return mav;
-}}
+    }
+
+    @GetMapping("/admin/delete/{idTour}")
+    @ResponseBody
+    public void removeTourByAdmin(@PathVariable Long idTour, HttpServletResponse response) {
+        try {
+                Tour tour = tourService.getTourById(idTour);
+                tourRepository.delete(tour);
+                response.sendRedirect("/admin/tours-manage");
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
